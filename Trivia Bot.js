abdfbaf516d164bf5bd1520143093849a93b64d2
Guntur-Thejaswini <thejaswinigunturu01@gmail.com>
2025-10-01 07:23:07 +0000

    Initial commit

---
+++ Trivia Bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Bot details
+let botName = "CodeWhizBot";
+let botLocation = "Tirupati";
+let favoriteLanguage = "JavaScript";
+
+// Introduction messages
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// Fun facts about the favorite language
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used both on the client-side and server-side of web development.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can be used to build games, apps, and even control robots!";
+console.log(codingFact);
+
+// Farewell message
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
